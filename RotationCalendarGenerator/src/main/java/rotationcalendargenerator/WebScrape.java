6a153f03ac448ce6db060e19df587971951e55ed
Rex-tc <rextcmisc@gmail.com>
2023-01-10 18:51:28 -0500

    rewrite projects

---
+++ RotationCalendarGenerator/src/main/java/rotationcalendargenerator/WebScrape.java
@@ -0,0 +1,10 @@
+package rotationcalendargenerator;
+
+import com.gargoylesoftware.htmlunit.*;
+import com.gargoylesoftware.htmlunit.html.*;
+import java.io.IOException;
+import java.util.List;
+
+public class WebScrape {
+    
+}
